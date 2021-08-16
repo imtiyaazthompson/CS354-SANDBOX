32548c7888ea0aace41aef305c3f7ff0ecff92e5
imtiyaazthompson <imtiyaaztom@gmail.com>
2021-08-16 13:07:11 +0200

    Removed unecessary imports

--- Server.java
+++ Server.java
@@ -1,10 +1,7 @@
-import java.io.BufferedReader;
 import java.util.Random;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
-import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.HashMap;
--- ServerThread.java
+++ ServerThread.java
@@ -1,10 +1,6 @@
-import java.io.BufferedInputStream;
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
-import java.io.PrintWriter;
 import java.net.Socket;
 
 public class ServerThread extends Thread {
